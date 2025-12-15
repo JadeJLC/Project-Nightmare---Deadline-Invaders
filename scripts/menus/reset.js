fc54d6e3ed358c8cd89494842bef7759c5926f8c
jcolange <jade.colanges@live.fr>
2025-12-15 16:34:03 +0100

    Correctif des boutons reset / restart

--- scripts/menus/reset.js
+++ scripts/menus/reset.js
@@ -11,6 +11,10 @@ import {
 } from "../variables.js";
 import { loadMainMenu } from "./main-menu.js";
 import { loadLevel } from "../engine/levels.js";
+import { disableEnemyShooting } from "../mechanics/enemy-shooting.js";
+import { disableMovement } from "../animations/player-movement.js";
+import { disableShooting } from "../mechanics/shooting.js";
+import { removeTimer, pauseTimer } from "../engine/timer.js";
 
 // Fonction pour redémarrer le jeu depuis le menu principal
 // ---- Réinitialise gameData et defaultLevel à leur valeur de départ et replace le joueur au centre de l'écran de jeu, puis ouvre le menu principal
@@ -25,6 +29,12 @@ function resetGame() {
 
   gameScreen.classList.add("is-hidden");
 
+  disableEnemyShooting();
+  disableMovement();
+  disableShooting();
+  pauseTimer();
+  removeTimer();
+
   loadMainMenu();
 }
 
@@ -37,6 +47,13 @@ function restartLevel() {
   gameData.score = lastLevel.score;
   gameData.lives = lastLevel.lives;
   gameData.powerups = lastLevel.powerups;
+
+  enemyLines.innerHTML = "";
+  disableEnemyShooting();
+  disableMovement();
+  disableShooting();
+  pauseTimer();
+  removeTimer();
 
   loadLevel();
 }
