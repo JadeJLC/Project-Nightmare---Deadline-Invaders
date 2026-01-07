74b886e79c03f34654c604d78e2bedfc53f3dba9
jcolange <jade.colanges@live.fr>
2026-01-07 14:33:11 +0100

    Finalisation des maps
    - Ajout du système de map dans les cinématiques
    - Ajout du système de map pour les powerups
    - Ajout du système de map pour les vies
    - Actuellement, deux équipes disponibles.
    - A faire : ajouter deux nouvelles équipes, adapter les cinématiques (mot neutre pour le patron)

---
+++ scripts/cutscenes/images-map.js
@@ -0,0 +1,91 @@
+const coworker1 = {
+  pose1: "0px -1008px",
+  pose2: "-144px -1008px",
+  pose3: "-288px -1008px",
+  pose4: "-432px -1008px",
+  party_pose1: "-192px -1008px",
+  party_pose2: "-240px - 1008px",
+  party_pose3: "-192px -1152px",
+  party_pose4: "-240px -1152px",
+};
+
+const coworker2 = {
+  pose1: "0px -1296px",
+  pose2: "-144px -1296px",
+  pose3: "-288px -1296px",
+  pose4: "-432px -1296px",
+  party_pose1: "-192px -1296px",
+  party_pose2: "-240px - 1296px",
+  party_pose3: "-192px -1440px",
+  party_pose4: "-240px -1440px",
+};
+
+const coworker3 = {
+  pose1: "0px -1584px",
+  pose2: "-144px -1584px",
+  pose3: "-288px -1584px",
+  pose4: "-432px -1584px",
+  party_pose1: "-192px -1584px",
+  party_pose2: "-240px - 1584px",
+  party_pose3: "-192px -1728px",
+  party_pose4: "-240px -1728px",
+};
+
+const coworker4 = {
+  pose1: "0px -1872px",
+  pose2: "-144px -1872px",
+  pose3: "-288px -1872px",
+  pose4: "-432px -1872px",
+  party_pose1: "-192px -1872px",
+  party_pose2: "-240px - 1872px",
+  party_pose3: "-192px -2016px",
+  party_pose4: "-240px -2016px",
+};
+
+const coworker5 = {
+  pose1: "0px -2160px",
+  pose2: "-144px -2160px",
+  pose3: "-288px -2160px",
+  pose4: "-432px -2160px",
+  party_pose1: "-192px -2160px",
+  party_pose2: "-240px - 2160px",
+  party_pose3: "-192px -2304px",
+  party_pose4: "-240px -2304px",
+};
+
+const relou = {
+  pose1: "0px -1728px",
+  pose2: "-144px -1728px",
+  pose3: "-288px -1728px",
+  pose4: "-432px -1728px",
+  party_pose1: "0px -2016px",
+  party_pose2: "-144px -2016px",
+  party_pose3: "-288px -2016px",
+  party_pose4: "-432px -2016px",
+};
+
+const boss = {
+  pose1: "0px -1152px",
+  pose2: "-144px -1152px",
+  pose3: "-288px -1152px",
+  pose4: "-432px -1152px",
+};
+
+const party = {
+  pose1: "0px -1440px",
+  pose2: "-144px -1440px",
+  pose3: "-288px -1440px",
+  pose4: "-432px -1440px",
+};
+
+const cutsceneMaps = {
+  coworker1,
+  coworker2,
+  coworker3,
+  coworker4,
+  coworker5,
+  relou,
+  boss,
+};
+
+export { cutsceneMaps };
